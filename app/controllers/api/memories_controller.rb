64dd5b15689fa458b430dda9a74f457ff2abc336
Vance Ng <vancejng@gmail.com>
2015-05-18 18:55:20 +0000

    Created User and Memory API structures

--- app/controllers/api/memories_controller.rb
+++ app/controllers/api/memories_controller.rb
@@ -1,30 +1,43 @@
 module API
   class MemoriesController < ApplicationController
+  respond_to :json
 
     def index
       @memories = Memory.all
+      respond_with @memories
     end
 
     def show
       @memory = Memory.find(params[:id])
-      @memory.to_json
+      respond_with @memory 
     end
 
     def new
       @memory = Memory.new
+      respond_with @memory 
     end
 
     def edit
+      @memory = Memory.find(params[:id])
+      respond_with @memory 
     end
 
     def create
       @memory = Memory.create(memory_params)
+      respond_with @memory 
+      # need to also respond with a failure message or 'false'
     end
 
     def update
+      @memory = Memory.find(params[:id])
+      @memory.update_attributes(movie_params)
+      respond_with @memory 
     end
 
     def destroy
+      @memory = Memory.find(params[:id])
+      @memory.destroy
+      respond_with @memory 
     end
 
     protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
+  constraints subdomain: 'api' do
+    namespace :api, path: '/', defaults: {format: :json} do
+      resources :memories, :users
+    end
+  end
+
   resources :users
 
   resources :memories 
 
-  constraints subdomain: 'api' do
-    namespace :api, path: '/' do
-      resources :memories
-    end
-  end
   # get 'users/show'
 
   # get 'users/new'
