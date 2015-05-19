91a32d8df00be5b8d19ad6cab3f3ec789234d80a
Sarah Clinton-Baker <sarahclintonbaker@gmail.com>
2015-05-19 19:21:22 +0000

    memory params hash clone

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -20,12 +20,12 @@ class MemoriesController < ApplicationController
     query = params[:memory][:latitude].to_s + ", " + params[:memory][:longitude].to_s
     location = Geocoder.search(query)
     city = location.first.city
-    memory_params[:city] = city
+    place_id = location.first.data["place_id"]
 
-    # @memory = Memory.create(memory_params)
-    @memory = Memory.create(title: params[:memory][:title], description: params[:memory][:description], category: params[:memory][:category], latitude: params[:memory][:latitude], longitude: params[:memory][:longitude], city: city, google_place_id: params[:memory][:google_place_id], media_url: params[:memory][:media_url])
-    binding.pry
-
+    new_params = memory_params.clone
+    new_params[:city] = city
+    new_params[:google_place_id] = place_id
+    @memory = Memory.create(new_params)
   end
 
   def update
@@ -39,28 +39,5 @@ class MemoriesController < ApplicationController
   def memory_params
     params.require(:memory).permit(:title, :description, :category, :latitude, :longitude, :city, :google_place_id, :media_url, :created_at, :updated_at)
   end
-
-  #     def find_city(latitude, longitude)
-  #   p latitude, longitude
-  #   query = latitude.to_s + "," + longitude.to_s 
-  #   result = Geocoder.search(query).first 
-  #   p result
-  #   puts "THIS IS A CITY:" 
-  #   if (result) 
-  #       return city = result.city
-  #       puts city
-  #   end
-  # end
-
-  # def create
-  #   p params
-  #   p params[:longitude], params[:latitude]
-  #   city = find_city(params[:longitude], params[:latitude])
-  #   puts "THIS IS A CITY:" 
-  #   p city
-    # @memory = Memory.create(title: params[:title], description: params[:description], category: params[:category], latitude: params[:latitude], longitude: params[:longitude], city: city, google_place_id: params[:google_place_id], media_url: params[:media_url])
-  # end
-
-
 
 end
