d135a87c65ac2beef01a10a80d94d1b8fc0fe000
Sarah Clinton-Baker <sarahclintonbaker@gmail.com>
2015-05-21 18:24:29 +0000

    Made Jquery Masonry plugin responsive

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,9 +18,23 @@
 
 
 $(function(){ $(document).foundation();
-
+  
+  $( window ).load( function()
+  {
+    var columns    = 3,
+        setColumns = function() { columns = $( window ).width() > 640 ? 3 : $( window ).width() > 320 ? 2 : 1; };
+ 
+    setColumns();
+    $( window ).resize( setColumns );
+ 
+    $( '#list' ).masonry(
+    {
+        itemSelector: '.item',
+        columnWidth:  function( containerWidth ) { return containerWidth / columns; }
+    });
 
 });
+  });
 
 //   function displayMemoryMarker(memoryId) {
 //     $.ajax({
